69205f6605d96053c01e3afc1140104afe59b256
Ramy hakam <pencilsoft1@gmail.com>
2021-06-24 21:07:10 +0200

    36-add-main-assets

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends  AbstractController
+{
+    /**
+     * @Route("/", name="home_page")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function Home()
+    {
+       return $this->render('base.html.twig');
+    }
+
+}
