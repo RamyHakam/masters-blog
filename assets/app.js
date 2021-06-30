03737c87c40b8c8d693f1a9be06d36d68e82b417
Ramy hakam <pencilsoft1@gmail.com>
2021-06-30 22:52:03 +0200

    50-webpacke-config-part1

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+import $ from 'jquery';
+global.$ = global.jQuery = $;
+import 'bootstrap';
+import 'dropzone'
+import 'bs-stepper';
+import 'select2';
+
+
+
+// start the Stimulus application
+import './bootstrap';
