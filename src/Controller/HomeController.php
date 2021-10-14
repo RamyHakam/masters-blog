2a5c6f6f757c48108fd1794acd285833c36450c1
Ramy hakam <pencilsoft1@gmail.com>
2021-10-14 21:55:38 +0200

    70-add-post-service

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,22 @@
 namespace App\Controller;
 
 
+use App\Service\PostService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends  AbstractController
 {
+    /**
+     * @var PostService
+     */
+    private $postService;
+
+    public function __construct(PostService $postService)
+    {
+        $this->postService = $postService;
+    }
+
     /**
      * @Route("/home", name="home_page")
      * @return \Symfony\Component\HttpFoundation\Response
---
+++ src/Service/PostService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use App\Contract\UploadFileInterface;
+
+class PostService
+{
+    /**
+     * @var UploadFileInterface
+     */
+    private $uploadFile;
+
+    public function __construct(LocalFileUploadService $uploadFile)
+    {
+        $this->uploadFile = $uploadFile;
+    }
+
+    public function createNewPost()
+    {
+
+    }
+
+    public function getAllPosts()
+    {
+
+    }
+
+
+    public function likePost()
+    {
+
+    }
+
+    public function commentOnPost()
+    {
+
+    }
+
+}
