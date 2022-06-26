24e36634da5285949482f2c1647d314508b9b7a3
Ramy Hakam <pencilsoft1@gmail.com>
2022-06-26 23:10:46 +0200

    195-submit login form

--- src/Controller/AccountAuthController.php
+++ src/Controller/AccountAuthController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Form\UserLoginType;
 use App\Service\AccountService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,10 +27,23 @@ class AccountAuthController extends  AbstractController
      * @Route("/login",name="login_page")
      * @return Response
      */
-    public function loginAction()
+    public function loginAction( Request  $request)
     {
         $form = $this->createForm(UserLoginType::class);
-
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            if($data['email'] ==='test@test.com'&&$data['password']==='test')
+            {
+                return $this->redirectToRoute('home_page');
+            }
+            else{
+                $this->addFlash('error','Invalid email or password');
+                return $this->render('login.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
+        }
         return $this->render('login.html.twig', [
             'form' => $form->createView()
         ]);
