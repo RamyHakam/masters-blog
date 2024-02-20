d52879a92983613bcc169af609837f701dc4a078
Ramy Hakam <pencilsoft1@gmail.com>
2024-02-20 22:17:26 +0100

    274- Add admin view page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Entity\AdminAccount;
+use App\Form\AccountType;
+use App\Form\AdminAccountType;
+use App\Service\UploadFileService;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/admin')]
 #[Security('is_granted("ROLE_ADMIN")')]
-class AdminController extends  AbstractController
+class AdminController extends AbstractController
 {
     #[Route(path: '/account_list', name: 'account_list')]
     public function listAction(): Response
@@ -23,4 +33,40 @@ class AdminController extends  AbstractController
         return $this->render('delete_request.html.twig');
     }
 
+    #[Route(path: '/add-admin', name: 'add_admin')]
+    #[Security('is_granted("ROLE_SUPER_ADMIN")')]
+    public function addNewAdmin(
+        Request                     $request,
+        UserPasswordHasherInterface $passwordHasher,
+        UploadFileService           $uploadFileService,
+        EntityManagerInterface      $entityManager,
+    ): Response
+    {
+        $form = $this->createForm(AdminAccountType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $adminPhoto = $form->get('userPhoto')->getData();
+                $adminPhotoName = $uploadFileService->upload($adminPhoto, UploadFileService::avatarType);
+                $plainPassword = $form->get('plainPassword')->getData();
+                /** @var AdminAccount $adminAccount */
+                $adminAccount = $form->getData();
+                $adminAccount->setPassword($passwordHasher->hashPassword($adminPhoto, $plainPassword));
+                //          $adminAccount->setAvatar($adminPhotoName);
+                $entityManager->persist($adminAccount);
+                $entityManager->flush();
+                // To do: Send reset password  email to the new admin
+                $this->addFlash('success', ' new Admin added successfully');
+                return $this->redirectToRoute('account_list');
+            } else {
+                $errors = $form->getErrors(true);
+                foreach ($errors as $error) {
+                    $this->addFlash('error', $error->getMessage());
+                }
+            }
+        }
+        return $this->render('add_admin.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
---
+++ src/Form/AdminAccountType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('plainPassword', RepeatedType::class,
+                [
+                    'mapped' => false,
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'The password fields must match.',
+                    'options' => ['attr' => ['class' => 'password-field']],
+                    'required' => true,
+                    'first_options' => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Retype Password'],
+                ])
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phone')
+            ->add('title')
+            ->add('address')
+            ->add('userPhoto', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Account::class,
+        ]);
+    }
+}
